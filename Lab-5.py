2dfea90f68dc5b52de22f37867b52f1d00a7c0c7
DeVarl1 <63013396+DeVarl1@users.noreply.github.com>
2023-01-25 20:32:53 +0300

    Update Lab-5.py
    Обновил работу в выходным файлом

--- Lab-5.py
+++ Lab-5.py
@@ -1,34 +1,38 @@
-import tkinter as tk
-from github import Github
 import os
+from github import Github
+import tkinter as tk
+from tkinter import filedialog
 
 def get_repo_info():
-    repo_name = repo_entry.get()
+    repo_name = entry.get()
     g = Github()
     repo = g.get_repo(repo_name)
     repo_owner = repo.owner
-    with open("repo_info.txt", "w") as f:
-        f.write("Name: " + repo.name + "\n")
-        f.write("Description: " + repo.description + "\n")
-        f.write("Language: " + repo.language + "\n")
-        f.write("URL: " + repo.html_url + "\n")
+    filepath = filedialog.asksaveasfilename(defaultextension=".txt",filetypes=[("Text files", "*.txt"),("All Files", "*.*")])
+    with open(filepath, "w") as f:
         f.write("Company: " + str(repo_owner.company) + "\n")
         f.write("Created at: " + str(repo_owner.created_at) + "\n")
         f.write("Email: " + str(repo_owner.email) + "\n")
         f.write("ID: " + str(repo_owner.id) + "\n")
-        f.write("Name: " + repo_owner.name + "\n")
+        f.write("Name: " + repo.name + "\n")
+        f.write("URL: " + repo.html_url + "\n")
+        if repo_owner.name is not None:
+            f.write("Name: " + repo_owner.name + "\n")
+        else:
+            f.write("Name: N/A\n")
         f.write("URL: " + repo_owner.url + "\n")
+    print("Информация о репозитории сохранена.")
 
 root = tk.Tk()
 root.title("Информация о репозитории Github")
 
-repo_label = tk.Label(root, text="Введите имя репозитория:")
-repo_label.pack()
+label = tk.Label(root, text="Введите имя репозитория Github в формате автор/имя (например facebook/react):")
+label.pack()
 
-repo_entry = tk.Entry(root)
-repo_entry.pack()
+entry = tk.Entry(root)
+entry.pack()
 
-get_info_button = tk.Button(root, text="Получить информацию", command=get_repo_info)
-get_info_button.pack()
+button = tk.Button(root, text="Сохранить информацию о репозитории", command=get_repo_info)
+button.pack()
 
 root.mainloop()
